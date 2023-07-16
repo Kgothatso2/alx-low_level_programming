c86daa1f5933b3b24e41252abbafb5ed09cb7e60
Kgothatso <kgothatso.m1997@gmail.com>
2023-07-16 14:28:04 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Mogale says "start"
+* Return: Always start with 0
+*/
+
+int main(void)
+
+{
+char q;
+
+q = 'z';
+
+while
+(q >= 'a') { putchar(q);
+q--;
+}
+
+
+putchar('\n');
+return (0);
+
+}
