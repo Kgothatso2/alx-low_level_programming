9f59412b521937a0cb1153ea9bf8881e09c85d03
Kgothatso <kgothatso.m1997@gmail.com>
2023-07-16 14:35:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Mogale says start!
+* Return: success 0
+*/
+
+int main(void)
+{
+char j;
+
+char h;
+
+j = 'a';
+
+h = 0;
+
+while
+(h < 10) { putchar(h + '0');
+h++;
+}
+
+while
+
+(j <= 'f') { putchar(j);
+j++;
+}
+
+putchar('\n');
+
+return (0);
+
+}
