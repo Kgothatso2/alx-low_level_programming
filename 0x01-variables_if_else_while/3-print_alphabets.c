e24606432056f1cb01dd296ba8136914d5644c2f
Kgothatso <kgothatso.m1997@gmail.com>
2023-07-16 14:06:22 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - k ays input
+* Return: fhdbydy 0
+*/
+
+int main(void)
+{
+char s;
+char c;
+
+s = 'a';
+c = 'A';
+
+while
+(s <= 'z') { putchar(s);
+s++;
+}
+while
+(c <= 'Z') { putchar(c);
+c++;
+}
+putchar('\n');
+
+return (0);
+
+}
