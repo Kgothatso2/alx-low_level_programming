6a39c1006e35227c6ff1cd229e935e05854ac29d
Kgothatso <kgothatso.m1997@gmail.com>
2023-07-19 13:20:06 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,19 @@
 #include "main.h"
-/**
- * _islower - detect lowercase and uppercase
- *
- * Description: use putchar to print
- *@c: collect alphabets
- * Return (1) if c is lower otherwise (0)
- *
- */
-int_islower(int c)
-{
-	char i;
-	int lower = 0;
 
-	for (i = 'a'; i <= 'z'; i++)
+
+/**
+ * _islower - checks if a character is lowercase
+ *
+ * Description: This function checks if the given character is lowercase.
+ * It returns 1 if the character is lowercase, and 0 otherwise.
+ *@c: The character to be checked
+ *
+ * Return 1 if the character is lowercase, 0 otherwise
+ */
+	int _islower(int c)
 	{
-		if (i == c)
-		{
-		lower = 1;
-		}
+		if (c >= 'a' && c <= 'z')
+			return (1);
+		else
+			return (0);
 	}
-	return (lower);
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 void print_alphabet(void);
 void print alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
